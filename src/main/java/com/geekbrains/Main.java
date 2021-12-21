e2ed3462122469cc9c878859eb54c7cbfcccb9dd
geekbrains <Sea_1994@inbox.ru>
2021-12-21 21:26:29 +0700

    я сделал домашнее задание

---
+++ src/main/java/com/geekbrains/Main.java
@@ -0,0 +1,53 @@
+package com.geekbrains;
+
+public class Main {
+    /*public static void main(String[] args) {
+        System.out.println("Привет Евгений");
+
+        byte b = 126;
+        short s = 1234;
+        int i = 10;
+        long l = 123123123123123123L;
+
+        float f = 12.3f;
+        double d = 14.5;
+        int a1 = 10;
+        int a2 = 15;
+
+        int sum = a1 + a2;
+
+        int inc = 1;
+        inc = inc + 1;
+
+
+
+        char c = '1';
+        boolean bool = false;
+
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(sum);
+        System.out.println(inc);
+    }*/
+    public static void main(String[] args) {
+        int a = 41;
+
+        if(a == 40) {
+            System.out.println("a больше 40");
+        } else {
+            System.out.println("а меньше 40");
+        }
+        boolean ternar = 10 > 5 ? true : false;
+        if(ternar) {
+            System.out.println();
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+    public  static void printValue() {
+        System.out.println("value");
+    }
+}
